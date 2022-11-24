b6bbdc2e3ba43ebdc2976a7c6ea554c73c38e153
YuriiKabo <108694627+YuriiKabo@users.noreply.github.com>
2022-11-24 09:05:47 +0000

    Deploying to gh-pages from @ YuriiKabo/goit-js-hw-09@6d4bf5ff492a34abca9231474c9c8fa5fd905c11 🚀

---
+++ 01-color-switcher.04d0f2b9.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=0;function o(){clearInterval(a),e.disabled=!1,t.disabled=!0}o(),e.addEventListener("click",(function(){a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",o);
+//# sourceMappingURL=01-color-switcher.04d0f2b9.js.map
