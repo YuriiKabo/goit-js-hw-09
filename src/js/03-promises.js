4b65470389ddaff855c4cdbfbf970879e52ec2af
YuriiKabo <yourrocksmirnov@gmail.com>
2022-11-28 00:06:38 +0200

    hw9 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import { Notify } from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  createNumbers(
+    Number(refs.delay.value),
+    Number(refs.step.value),
+    Number(refs.amount.value)
+  );
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function createNumbers(delay, step, amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    delay += i === 1 ? 0 : step;
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`, {
+          timeout: 2000,
+        });
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`, {
+          timeout: 2000,
+        });
+      });
   }
 }
